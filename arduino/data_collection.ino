81ca1cc064e5257ae5386a726ef576469c2bef7a
delee5695 <delee5695@naver.com>
2025-04-29 00:11:02 -0400

    define sensor variables globally

--- arduino/data_collection.ino
+++ arduino/data_collection.ino
@@ -1,6 +1,14 @@
 // Data collection of the EMG and Pressure sensors 
-
 #include <Servo.h>
+
+// Global sensor variables
+int emgValue;
+int frontLeft1Value;
+int frontLeft2Value;
+int frontRight1Value;
+int frontRight2Value;
+int backLeftValue;
+int backRightValue;
 
 void setup() {
   Serial.begin(9600);
@@ -15,29 +23,22 @@ void loop() {
 
 // Function to read the EMG and Pressure sensor values
 void readSensors() {
-  int emgValue = analogRead(A1);
-  int frontLeft1Value = analogRead(A2); // Pressure sensor in front left toe
-  int frontLeft2Value = analogRead(A3); // Pressure sensor in front left middle
-  int frontRight1Value = analogRead(A4); // Pressure sensor in front right toe
-  int frontRight2Value = analogRead(A5); // Pressure sensor in front right middle
-  int backLeftValue = analogRead(A6); // Pressure sensor in back left toe
-  int backRightValue = analogRead(A7); // Pressure sensor in back right toe
+  emgValue = analogRead(A1);
+  frontLeft1Value = analogRead(A2); // Pressure sensor in front left toe
+  frontLeft2Value = analogRead(A3); // Pressure sensor in front left middle
+  frontRight1Value = analogRead(A4); // Pressure sensor in front right toe
+  frontRight2Value = analogRead(A5); // Pressure sensor in front right middle
+  backLeftValue = analogRead(A6); // Pressure sensor in back left toe
+  backRightValue = analogRead(A7); // Pressure sensor in back right toe
 }
 
 // Function to print the data to the serial monitor
 void serialPrintData() {
-  Serial.print(emgValue);
-  Serial.print(',');
-  Serial.print(pressureA2);
-  Serial.print(',');
-  Serial.print(pressureA3);
-  Serial.print(',');
-  Serial.print(pressureA4);
-  Serial.print(',');
-  Serial.print(pressureA5);
-  Serial.print(',');
-  Serial.print(pressureA6);
-  Serial.print(',');
-  Serial.println(pressureA7);
-  
+  Serial.print(emgValue); Serial.print(',');
+  Serial.print(frontLeft1Value); Serial.print(',');
+  Serial.print(frontLeft2Value); Serial.print(',');
+  Serial.print(frontRight1Value); Serial.print(',');
+  Serial.print(frontRight1Value); Serial.print(',');
+  Serial.print(backLeftValue); Serial.print(',');
+  Serial.println(backRightValue);
 }
