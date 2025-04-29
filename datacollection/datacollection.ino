ec34565a605047ccd67b3378a78fd40223fb00b1
delee5695 <delee5695@naver.com>
2025-04-28 22:31:08 -0400

    update datacollection.ino to include 6 pressure sensors

--- datacollection/datacollection.ino
+++ datacollection/datacollection.ino
@@ -1,33 +1,77 @@
-// Arduino sketch to read pressure sensor values from A0, A1, and A2
-// only when bump switch is activated
+#include <Servo.h>
 
-const int sensorPinA0 = A0;  // Pressure sensor 1
-const int sensorPinA1 = A1;  // Pressure sensor 2
-const int sensorPinA2 = A2;  // Pressure sensor 3
-const int bumpSwitchPin = 8; // Bump switch
+// Code tested for second sprint review.
+// Can move motors based on rock, paper, scissors movement
+
+// Define servo objects
+Servo frontLeftServo;
+Servo frontRightServo;
+Servo backLeftServo;
+Servo backRightServo;
+
+// Places where servos are connected
+const int frontLeftPin = 7; 
+const int frontRightPin = 5;
+const int backLeftPin = 8;
+const int backRightPin = 6;
+const int THRESHOLD = 150; 
+const int OPEN_POS = 0;     // Servo open position
+const int CLOSED_POS = 180;  // Servos closed position
+
+const int sensorPinA2 = A2;  // Pressure sensor 1
+const int sensorPinA3 = A3;  // Pressure sensor 2
+const int sensorPinA4 = A4;  // Pressure sensor 3
+const int sensorPinA5 = A5;  // Pressure sensor 1
+const int sensorPinA6 = A6;  // Pressure sensor 2
+const int sensorPinA7 = A7;  // Pressure sensor 3
 
 void setup() {
+  // Attach servos to their ports
+  frontLeftServo.attach(frontLeftPin);
+  frontRightServo.attach(frontRightPin);
+  backLeftServo.attach(backLeftPin);
+  backRightServo.attach(backRightPin);
+  pinkyServo.attach(pinkyPin);
+
+  // Set all servos to open
+  frontLeftServo.write(OPEN_POS);
+  frontRightServo.write(OPEN_POS);
+  backLeftServo.write(OPEN_POS);
+  backRightServo.write(OPEN_POS);
+  pinkyServo.write(OPEN_POS);
+
   Serial.begin(9600);
-  pinMode(bumpSwitchPin, INPUT);
 }
 
 void loop() {
-  int bumpState = digitalRead(bumpSwitchPin);
+  int pressureA2 = analogRead(sensorPinA2);
+  int pressureA3 = analogRead(sensorPinA3);
+  int pressureA4 = analogRead(sensorPinA4);
+  int pressureA5 = analogRead(sensorPinA5);
+  int pressureA6 = analogRead(sensorPinA6);
+  int pressureA7 = analogRead(sensorPinA7);
+  int emgValue = analogRead(A0);
+  collectEMGData();
+  collectPressureData();
+  delay(500);
+}
 
-  if (bumpState == LOW) { // Bump switch is activated
-    int pressureA0 = analogRead(sensorPinA0);
-    int pressureA1 = analogRead(sensorPinA1);
-    int pressureA2 = analogRead(sensorPinA2);
+// Function to show Paper gesture
+void collectPressureData() {
+  Serial.print(pressureA2);
+  Serial.print(',');
+  Serial.print(pressureA3);
+  Serial.print(',');
+  Serial.print(pressureA4);
+  Serial.print(',');
+  Serial.print(pressureA5);
+  Serial.print(',');
+  Serial.print(pressureA6);
+  Serial.print(',');
+  Serial.println(pressureA7);
+}
 
-    Serial.print("Pressure Sensor Data ");
-    Serial.print(pressureA0);
-    Serial.print(',');
-    Serial.print(pressureA1);
-    Serial.print(',');
-    Serial.println(pressureA2);
-  } else {
-    Serial.println("Bump switch not activated. Sensors inactive.");
-  }
-
-  delay(5); 
+void collectEMGData(){
+  Serial.print(emgValue);
+  Serial.print(',');
 }
