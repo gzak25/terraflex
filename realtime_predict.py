e0864f35a030c72e959761a9769fba22a6604f65
delee5695 <delee5695@naver.com>
2025-04-29 00:39:39 -0400

    change realtimepredict file name

--- realtime_predict.py
+++ realtime_predict.py
@@ -1,60 +1,20 @@
+import time
 import torch
 import numpy as np
 import serial
-import torch.nn as nn
-import torch.nn.functional as F
-import time
+from cnn import CNN
 
 SERIAL_PORT = "/dev/ttyACM0"
 BAUD_RATE = 9600
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
 
-window_size = 20  # Adjust based on how often you want predictions to happen
+WINDOW_SIZE = 20  # Adjust based on how often you want predictions to happen
 
 
-class CNN(nn.Module):
-    def __init__(self, num_classes):
-
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv1d(3, 32, kernel_size=5, stride=1, padding=2)
-        self.bn1 = nn.BatchNorm1d(32)
-
-        self.conv2 = nn.Conv1d(32, 64, kernel_size=5, stride=1, padding=2)
-        self.bn2 = nn.BatchNorm1d(64)
-
-        self.conv3 = nn.Conv1d(64, 128, kernel_size=5, stride=1, padding=2)
-        self.bn3 = nn.BatchNorm1d(128)
-
-        self.conv4 = nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm1d(128)
-
-        self.pool = nn.AdaptiveAvgPool1d(1)
-        self.dropout = nn.Dropout(0.4)
-        self.fc1 = nn.Linear(128, 64)
-        self.fc2 = nn.Linear(64, num_classes)
-
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.max_pool1d(x, kernel_size=2)
-
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.max_pool1d(x, kernel_size=2)
-
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
-
-        x = F.relu(self.fc1(self.dropout(x)))
-        x = self.fc2(x)
-        return x
-
-
-model_path = "model.pth"
-num_classes = 5  # Categories: stable, forward, right, left, back
-model_cnn = CNN(num_classes=num_classes)
-model_cnn.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
+MODEL_PATH = "model.pth"
+NUM_CLASSES = 5  # Categories: stable, forward, right, left, back
+model_cnn = CNN(num_classes=NUM_CLASSES)
+model_cnn.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device("cpu")))
 model_cnn.eval()
 
 # Balance mapping
@@ -121,12 +81,11 @@ if __name__ == "__main__":
             if sensor_values is not None:
                 sensor_buffer.append(sensor_values)
                 # Predict balance when we have enough data
-                if len(sensor_buffer) >= window_size:
-                    predicted_label = predict_from_buffer(sensor_buffer[:window_size])
+                if len(sensor_buffer) >= WINDOW_SIZE:
+                    predicted_label = predict_from_buffer(sensor_buffer[:WINDOW_SIZE])
                     print(f"Predicted: {predicted_label}")
                     send_to_arduino(predicted_label)
                     sensor_buffer = []
-
             else:
                 continue
     except KeyboardInterrupt:
